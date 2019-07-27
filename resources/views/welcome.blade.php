e1d02f74ce02f12b0aafe98992ccc8cc6e4a2f7c
Rubén Méndez <rmendez@kairos.systems>
2019-07-27 16:37:03 -0500

    changes in view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,12 +85,12 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://laravel.com/docs">Documents</a>
+                    <a href="https://laracasts.com">Laracasts Page</a>
+                    <a href="https://laravel-news.com">Newspaper</a>
+                    <a href="https://blog.laravel.com">Blog Info</a>
+                    <a href="https://nova.laravel.com">Supernova</a>
+                    <a href="https://forge.laravel.com">Forget</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
